3ce669bb5ef06a7f54dc825757c6f4f1dca80217
Warcrea <andrewstockdale1@gmail.com>
2017-03-03 19:18:51 +0000

    Input & Output

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public string inputKey;
+    public string outputKey;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(inputKey)){
+            Debug.Log(outputKey);
+        }
+	}
+}
