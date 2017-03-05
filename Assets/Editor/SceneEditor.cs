97943e6dcbdb8940a8210eb93dafbb6f54b2bd4e
Warcrea <andrewstockdale1@gmail.com>
2017-03-05 11:51:11 +0000

    Scene and Dialogue Entry

---
+++ Assets/Editor/SceneEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(Scene))]
+public class LevelDataEditor : Editor
+{
+    private ReorderableList list;
+
+    private void OnEnable()
+    {
+        list = new ReorderableList(serializedObject,
+                serializedObject.FindProperty("moments"),
+                true, true, true, true);
+
+        list.drawElementCallback =
+        (Rect rect, int index, bool isActive, bool isFocused) => {
+            var element = list.serializedProperty.GetArrayElementAtIndex(index);
+            rect.y += 2;
+            EditorGUI.PropertyField(
+                new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("actor"), GUIContent.none);
+            EditorGUI.PropertyField(
+                new Rect(rect.x + 60, rect.y, rect.width - 60 - 30, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("dialogue"), GUIContent.none);
+            EditorGUI.PropertyField(
+                new Rect(rect.x + rect.width - 30, rect.y, 30, EditorGUIUtility.singleLineHeight),
+                element.FindPropertyRelative("timeStart"), GUIContent.none);
+        };
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        list.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ Assets/Scripts/Level Dialogues/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct Dialogue
+{
+    public int actor;   //Who is saying the line
+    public int timeStart;   //The time after the previous comment this line should come
+    public int duration;   //How long the line stays onscreen
+    public string dialogue;     //What the actual text is
+    public string representation;   //How it should be represented
+    public bool marked; //If the player is marked down for not including this
+}
